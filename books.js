4d6588b2e85f56f08c67f5a7f2949a2619085ae4
starzlocker <adolfo.gomes@al.infnet.edu.br>
2024-09-13 19:22:29 -0300

    o

---
+++ books.js
@@ -0,0 +1,88 @@
+let books = [
+  {
+      "title": "1984",
+      "author": "George Orwell",
+      "year": "1949",
+      "genre": "Dystopian",
+      "cover": "./1984.jpg"
+  },
+  {
+      "title": "To Kill a Mockingbird",
+      "author": "Harper Lee",
+      "year": "1960",
+      "genre": "Fiction",
+      "cover": "./to_kill_a_mockingbird.jpg"
+  },
+  {
+      "title": "The Great Gatsby",
+      "author": "F. Scott Fitzgerald",
+      "year": "1925",
+      "genre": "Tragedy",
+      "cover": "./the_great_gatsby.jpg"
+  },
+  {
+      "title": "Pride and Prejudice",
+      "author": "Jane Austen",
+      "year": "1813",
+      "genre": "Romance",
+      "cover": "./pride_and_prejudice.jpg"
+  },
+  {
+      "title": "The Catcher in the Rye",
+      "author": "J.D. Salinger",
+      "year": "1951",
+      "genre": "Fiction",
+      "cover": "./the_catcher_in_the_rye.jpg"
+  },
+  {
+      "title": "Moby Dick",
+      "author": "Herman Melville",
+      "year": "1851",
+      "genre": "Adventure",
+      "cover": "./moby_dick.jpg"
+  },
+  {
+      "title": "Brave New World",
+      "author": "Aldous Huxley",
+      "year": "1932",
+      "genre": "Dystopian",
+      "cover": "./brave_new_world.jpg"
+  },
+  {
+      "title": "War and Peace",
+      "author": "Leo Tolstoy",
+      "year": "1869",
+      "genre": "Historical Fiction",
+      "cover": "./war_and_peace.jpg"
+  },
+  {
+      "title": "Crime and Punishment",
+      "author": "Fyodor Dostoevsky",
+      "year": "1866",
+      "genre": "Psychological Fiction",
+      "cover": "./crime_and_punishment.jpg"
+  },
+  {
+      "title": "The Hobbit",
+      "author": "J.R.R. Tolkien",
+      "year": "1937",
+      "genre": "Fantasy",
+      "cover": "./the_hobbit.jpg"
+  },
+  {
+      "title": "Fahrenheit 451",
+      "author": "Ray Bradbury",
+      "year": "1953",
+      "genre": "Dystopian",
+      "cover": "./fahrenheit_451.jpg"
+  },
+  {
+      "title": "The Lord of the Rings",
+      "author": "J.R.R. Tolkien",
+      "year": "1954",
+      "genre": "Fantasy",
+      "cover": "./the_lord_of_the_rings.jpg"
+  }
+];
+
+export default books;
