ca478ed897ac4895a65886ced45e514d48a54dc2
starzlocker <adolfo.gomes@al.infnet.edu.br>
2024-09-13 16:47:03 -0300

    Correções na localização dos folders

--- src/data/books.js
+++ src/data/books.js
@@ -4,84 +4,84 @@ let books = [
       "author": "George Orwell",
       "year": "1949",
       "genre": "Dystopian",
-      "cover": "./midia/1984.jpg"
+      "cover": "public/1984.jpg"
   },
   {
       "title": "To Kill a Mockingbird",
       "author": "Harper Lee",
       "year": "1960",
       "genre": "Fiction",
-      "cover": "./midia/to_kill_a_mockingbird.jpg"
+      "cover": "public/to_kill_a_mockingbird.jpg"
   },
   {
       "title": "The Great Gatsby",
       "author": "F. Scott Fitzgerald",
       "year": "1925",
       "genre": "Tragedy",
-      "cover": "./midia/the_great_gatsby.jpg"
+      "cover": "public/the_great_gatsby.jpg"
   },
   {
       "title": "Pride and Prejudice",
       "author": "Jane Austen",
       "year": "1813",
       "genre": "Romance",
-      "cover": "./midia/pride_and_prejudice.jpg"
+      "cover": "public/pride_and_prejudice.jpg"
   },
   {
       "title": "The Catcher in the Rye",
       "author": "J.D. Salinger",
       "year": "1951",
       "genre": "Fiction",
-      "cover": "./midia/the_catcher_in_the_rye.jpg"
+      "cover": "public/the_catcher_in_the_rye.jpg"
   },
   {
       "title": "Moby Dick",
       "author": "Herman Melville",
       "year": "1851",
       "genre": "Adventure",
-      "cover": "./midia/moby_dick.jpg"
+      "cover": "public/moby_dick.jpg"
   },
   {
       "title": "Brave New World",
       "author": "Aldous Huxley",
       "year": "1932",
       "genre": "Dystopian",
-      "cover": "./midia/brave_new_world.jpg"
+      "cover": "public/brave_new_world.jpg"
   },
   {
       "title": "War and Peace",
       "author": "Leo Tolstoy",
       "year": "1869",
       "genre": "Historical Fiction",
-      "cover": "./midia/war_and_peace.jpg"
+      "cover": "public/war_and_peace.jpg"
   },
   {
       "title": "Crime and Punishment",
       "author": "Fyodor Dostoevsky",
       "year": "1866",
       "genre": "Psychological Fiction",
-      "cover": "./midia/crime_and_punishment.jpg"
+      "cover": "public/crime_and_punishment.jpg"
   },
   {
       "title": "The Hobbit",
       "author": "J.R.R. Tolkien",
       "year": "1937",
       "genre": "Fantasy",
-      "cover": "./midia/the_hobbit.jpg"
+      "cover": "public/the_hobbit.jpg"
   },
   {
       "title": "Fahrenheit 451",
       "author": "Ray Bradbury",
       "year": "1953",
       "genre": "Dystopian",
-      "cover": "./midia/fahrenheit_451.jpg"
+      "cover": "public/fahrenheit_451.jpg"
   },
   {
       "title": "The Lord of the Rings",
       "author": "J.R.R. Tolkien",
       "year": "1954",
       "genre": "Fantasy",
-      "cover": "./midia/the_lord_of_the_rings.jpg"
+      "cover": "public/the_lord_of_the_rings.jpg"
   }
 ];
 
